bae62f8add787ffcd6fb040bdaace3d6b0d70739
Melany <melanyjsuarez@gmail.com>
2025-06-07 00:16:27 -0500

    Feature PT 2

--- src/main/java/com/maxiguias/maxigestion/maxigestion/modelo/PersonaInfo.java
+++ src/main/java/com/maxiguias/maxigestion/maxigestion/modelo/PersonaInfo.java
@@ -1,46 +1,19 @@
 package com.maxiguias.maxigestion.maxigestion.modelo;
-
 import jakarta.persistence.Embeddable;
+import lombok.Data;
 
 @Embeddable
+@Data
 public class PersonaInfo {
 
     private String nombre;
     private String direccion;
-    private String telefono; // Using String to avoid numeric overflow
+    private String telefono; 
 
-    // Default constructor
-    public PersonaInfo() {}
-
-    // Constructor with parameters
     public PersonaInfo(String nombre, String direccion, String telefono) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
     }
 
-    // Getters and Setters
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 }
