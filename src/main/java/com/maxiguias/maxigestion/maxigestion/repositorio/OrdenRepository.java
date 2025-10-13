a881427c17c6d67ed9073865016721e169bc556b
Melany <melanyjsuarez@gmail.com>
2025-10-13 18:16:19 -0500

    Fix ordenes

--- src/main/java/com/maxiguias/maxigestion/maxigestion/repositorio/OrdenRepository.java
+++ src/main/java/com/maxiguias/maxigestion/maxigestion/repositorio/OrdenRepository.java
@@ -14,10 +14,10 @@ public interface OrdenRepository extends JpaRepository<Orden, Long> {
     Long countBy();
 
     // Métodos para reportes con filtro por mes
-    @Query("SELECT COUNT(o) FROM Orden o WHERE MONTH(o.fechaVenta) = :mes AND YEAR(o.fechaVenta) = :anio")
+    @Query("SELECT COUNT(o) FROM Orden o WHERE MONTH(o.fechaOrden) = :mes AND YEAR(o.fechaOrden) = :anio")
     Long countByMes(@Param("mes") Integer mes, @Param("anio") Integer anio);
 
-    @Query("SELECT o FROM Orden o WHERE MONTH(o.fechaVenta) = :mes AND YEAR(o.fechaVenta) = :anio")
+    @Query("SELECT o FROM Orden o WHERE MONTH(o.fechaOrden) = :mes AND YEAR(o.fechaOrden) = :anio")
     List<Orden> findByMes(@Param("mes") Integer mes, @Param("anio") Integer anio);
 
 }
