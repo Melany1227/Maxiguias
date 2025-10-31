e662031d45c9a89adae775413c9969aa72e69554
Wilmer-Ruiz <wilmergruiz14@gmail.com>
2025-10-31 11:17:40 -0500

    Agg ganancia x mayor y encargo

--- src/main/java/com/maxiguias/maxigestion/maxigestion/controlador/ReporteController.java
+++ src/main/java/com/maxiguias/maxigestion/maxigestion/controlador/ReporteController.java
@@ -415,9 +415,12 @@ public class ReporteController {
             headerFont.setBold(true);
             headerStyle.setFont(headerFont);
 
+            CellStyle percentageStyle = workbook.createCellStyle();
+            percentageStyle.setDataFormat(workbook.createDataFormat().getFormat("0%"));
+
             // Crear fila de encabezado
             Row headerRow = sheet.createRow(0);
-            String[] headers = {"Nombre del Producto", "Medida del Producto", "Precio por Mayor", "Precio por Encargo"};
+            String[] headers = {"Nombre del Producto",  "Medida del Producto", "Precio Público", "Precio por Mayor", "% Ganancia al por Mayor", "Precio por Encargo", "% Ganancia por Encargo"};
 
             for (int i = 0; i < headers.length; i++) {
                 Cell cell = headerRow.createCell(i);
@@ -429,10 +432,25 @@ public class ReporteController {
             int rowNum = 1;
             for (Terminado terminado : terminados) {
                 Row row = sheet.createRow(rowNum++);
+
+                double precioPublico = terminado.getPrecioPublico();
+                double precioMayor = terminado.getPrecioPorMayor();
+                double precioEncargo = terminado.getPrecioPorEncargo();
+                
+                double gananciaMayorPct = (precioPublico > 0) ? (precioPublico - precioMayor) / precioPublico : 0;
+                double gananciaEncargoPct = (precioPublico > 0 ) ? precioEncargo / precioPublico : 0;
+
                 row.createCell(0).setCellValue(terminado.getProducto().getNombre());
                 row.createCell(1).setCellValue(terminado.getMedidaTerminadoProducto().doubleValue());
-                row.createCell(2).setCellValue(terminado.getPrecioPorMayor());
-                row.createCell(3).setCellValue(terminado.getPrecioPorEncargo());
+                row.createCell(2).setCellValue(terminado.getPrecioPublico());
+                row.createCell(3).setCellValue(terminado.getPrecioPorMayor());
+                Cell cellGananciaMayor = row.createCell(4);
+                cellGananciaMayor.setCellValue(gananciaMayorPct);
+                cellGananciaMayor.setCellStyle(percentageStyle);
+                row.createCell(5).setCellValue(terminado.getPrecioPorEncargo());
+                Cell cellGananciaEncargo = row.createCell(6);
+                cellGananciaEncargo.setCellValue(gananciaEncargoPct);
+                cellGananciaEncargo.setCellStyle(percentageStyle);
             }
 
             // Ajustar el ancho de las columnas
