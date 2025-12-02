80e5a3472eb9adff9b837c41ab084ff396af94eb
Wilmer-Ruiz <wilmergruiz14@gmail.com>
2025-12-01 23:25:31 -0500

    Fix ayuda y acerca, validaciones ciudad, departamento y rol

--- src/main/java/com/maxiguias/maxigestion/maxigestion/repositorio/CiudadRepository.java
+++ src/main/java/com/maxiguias/maxigestion/maxigestion/repositorio/CiudadRepository.java
@@ -5,9 +5,13 @@ import java.util.List;
 import com.maxiguias.maxigestion.maxigestion.modelo.Ciudad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CiudadRepository extends JpaRepository<Ciudad, Integer> {
     
     List<Ciudad> findByDepartamento_Id(Integer departamentoId);
+
+    Optional<Ciudad> findByNombre(String nombre);
     
 }
 
--- src/main/java/com/maxiguias/maxigestion/maxigestion/repositorio/RolRepository.java
+++ src/main/java/com/maxiguias/maxigestion/maxigestion/repositorio/RolRepository.java
@@ -2,8 +2,22 @@ package com.maxiguias.maxigestion.maxigestion.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 import com.maxiguias.maxigestion.maxigestion.modelo.Rol;
 
+
+
+import java.util.Optional;
+
+
+
 public interface RolRepository extends JpaRepository<Rol, Long> {
+
+
+
+    Optional<Rol> findByNombreRol(String nombreRol);
+
+
 
 }
